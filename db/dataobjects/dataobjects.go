739b2e2aab19313bc71b17d3178b40a5efa43976
Camilo Pestana <capcarde@gmail.com>
2017-08-25 13:46:58 +0800

    Update dataobjects.go

--- db/dataobjects/dataobjects.go
+++ db/dataobjects/dataobjects.go
@@ -3,7 +3,7 @@ package dataobjects
 import "time"
 
 type User struct {
-	Id			int			`gorm:"primary_key; AUTO_INCREMENT" json:"id"`
+	Id		int		`gorm:"primary_key; AUTO_INCREMENT" json:"id"`
 	Username	string		`gorm:"size:255" json:"username"`
 	Email		string		`gorm:"size:100" json:"email"`
 	Password	string		`json:"password"`
@@ -12,20 +12,20 @@ type User struct {
 	CreatedAt	time.Time	`json:"createdAt"`
 	UpdatedAt 	time.Time	`json:"updatedAt"`
 	Country		Country		`gorm:"ForeignKey:Id;AssociationForeignKey:CountryId" json:"country"` //ISO "ALPHA-2 Code"
-	CountryId	int			`json:"country_id,omitempty"`
+	CountryId	int		`json:"country_id,omitempty"`
 	City		Country		`gorm:"ForeignKey:Id;AssociationForeignKey:CityId" json:"city"` //ISO "ALPHA-2 Code"
-	CityId		int			`json:"city_id,omitempty"`
+	CityId		int		`json:"city_id,omitempty"`
 }
 
 type Country struct {
-	Id		int	`gorm:"primary_key;AUTO_INCREMENT" json:"id"`
+	Id	int	`gorm:"primary_key;AUTO_INCREMENT" json:"id"`
 	Name	string	`gorm:"size:150" json:"name"`
 }
 
 
 type City struct {
-	Id			int		`gorm:"primary_key;AUTO_INCREMENT" json:"id"`
-	Name		string	`gorm:"size:150"`
+	Id		int		`gorm:"primary_key;AUTO_INCREMENT" json:"id"`
+	Name		string		`gorm:"size:150"`
 	CountryId	int		`json:"countryId"`
 }
 
